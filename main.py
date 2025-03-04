fb08d5364834eea9a9b1ab7b0a293e8eb101bef9
Ruhany Aragao <ruhanyaragao@Ruhanys-Mac-mini.local>
2025-03-04 08:27:46 -0300

    Allowing all origins

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://172.16.21.7:3000", "http://localhost:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
